21390cfd34e7de265eb1dda03a4595d6f07fc6df
songmm <songmm@qingsongjituan.com>
2025-12-13 14:19:56 +0800

    fix：修复图片裁剪

--- src/shared/components/PictureCropper.tsx
+++ src/shared/components/PictureCropper.tsx
@@ -1,9 +1,9 @@
+/* eslint-disable @next/next/no-img-element */
 'use client'
 
 import clsx from 'clsx'
 import dynamic from 'next/dynamic'
-import Image from 'next/image'
-import { useEffect, useRef, useState } from 'react'
+import { useCallback, useRef, useState } from 'react'
 import { type PercentCrop, type PixelCrop, type Crop } from 'react-image-crop'
 import 'react-image-crop/dist/ReactCrop.css'
 import { twMerge } from 'tailwind-merge'
@@ -14,60 +14,62 @@ type PictureCropperProps = {
   className?: string
   size?: number
   aspectRatio?: number
-  cropSize?: number
   onCropComplete?: (blob: Blob) => void
 }
 
 const PictureCropper = (props: PictureCropperProps) => {
-  const { src, className, size = 400, aspectRatio = 1, cropSize = 120, onCropComplete } = props
+  const { src, className, size = 400, aspectRatio = 1, onCropComplete } = props
   const [crop, setCrop] = useState<Crop>()
   const imageRef = useRef<HTMLImageElement>(null)
 
-  useEffect(() => {
-    return () => {}
-  }, [])
-
-  const handleImageLoad = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+  const handleImageLoad = useCallback((e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+    const { offsetWidth, offsetHeight } = e.currentTarget
+    const cropSize = Math.min(offsetWidth, offsetHeight) * 0.8
+    const x = Math.floor((offsetWidth - cropSize) / 2)
+    const y = Math.floor((offsetHeight - cropSize) / 2)
     setCrop({
       unit: 'px',
       width: cropSize,
       height: cropSize,
-      x: (size - cropSize) / 2,
-      y: (size - cropSize) / 2
+      x: x,
+      y: y
     })
-  }
+  }, [])
 
-  const handleOnComplete = (crop: PixelCrop, _percentageCrop: PercentCrop) => {
-    if (!crop || !crop.width || !crop.height || !imageRef.current) return
-    const canvas = document.createElement('canvas')
-    canvas.width = crop.width
-    canvas.height = crop.height
-    const ctx = canvas.getContext('2d')
-    if (!ctx) return
-    const image = imageRef.current
-    const { naturalWidth, naturalHeight, offsetWidth, offsetHeight } = image
-    const scaleX = naturalWidth / offsetWidth
-    const scaleY = naturalHeight / offsetHeight
-    ctx.drawImage(
-      image,
-      crop.x * scaleX,
-      crop.y * scaleY,
-      crop.width * scaleX,
-      crop.height * scaleY,
-      0,
-      0,
-      crop.width,
-      crop.height
-    )
+  const handleOnComplete = useCallback(
+    (crop: PixelCrop, _percentageCrop: PercentCrop) => {
+      if (!crop || !crop.width || !crop.height || !imageRef.current) return
+      const canvas = document.createElement('canvas')
+      canvas.width = crop.width
+      canvas.height = crop.height
+      const ctx = canvas.getContext('2d')
+      if (!ctx) return
+      const image = imageRef.current
+      const { naturalWidth, naturalHeight, offsetWidth, offsetHeight } = image
+      const scaleX = naturalWidth / offsetWidth
+      const scaleY = naturalHeight / offsetHeight
+      ctx.drawImage(
+        image,
+        crop.x * scaleX,
+        crop.y * scaleY,
+        crop.width * scaleX,
+        crop.height * scaleY,
+        0,
+        0,
+        crop.width,
+        crop.height
+      )
 
-    canvas.toBlob(
-      (blob) => {
-        if (blob) onCropComplete?.(blob)
-      },
-      'image/jpeg',
-      1
-    )
-  }
+      canvas.toBlob(
+        (blob) => {
+          if (blob) onCropComplete?.(blob)
+        },
+        'image/jpeg',
+        1
+      )
+    },
+    [onCropComplete]
+  )
 
   return (
     <ReactCropDynamic
@@ -75,11 +77,11 @@ const PictureCropper = (props: PictureCropperProps) => {
       crop={crop}
       locked={false}
       circularCrop={false}
-      onChange={(_, percentCrop) => setCrop(percentCrop)}
+      onChange={(crop) => setCrop(crop)}
       aspect={aspectRatio}
       onComplete={handleOnComplete}
     >
-      <Image
+      <img
         ref={imageRef}
         className={clsx(twMerge('object-cover w-full h-full'))}
         src={src}
